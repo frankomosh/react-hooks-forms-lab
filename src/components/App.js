0a49a7d663edd075f1601e580c850cfe0db11471
Frank-197 <frankomosh197@gmail.com>
2022-07-25 08:45:51 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,27 @@ import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
 import itemData from "../data/items";
-
+import Filter from "./Filter";
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
-
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function handlingAddItems(newItem) {
+        // console.log(newItem);
+    setItems([...items, newItem]);
+    
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
+      {/* <Filter /> */}
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={handlingAddItems} />
     </div>
   );
 }
-
 export default App;
+
